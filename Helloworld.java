80bcd838aff9abf2b2516ddc38ac1ae04a97f1c0
PeddaseshugallaPadma <105662709+peddaseshugallapadma@users.noreply.github.com>
2022-06-03 22:09:35 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+package Java_Course;
+
+public class Helloworld {
+    public static void main(String[] args) {
+        // System.out.print("Hello Paramesh");
+        System.out.println("Hello World");
+    }
+}
